1ed8c0292710bbb5d40e5fd9edaf5883758e2332
yifanyang96 <yifanyang96@outlook.com>
2018-01-27 14:35:19 +0800

    first commit

---
+++ fd-net-auth.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+URL="https://10.108.255.249/include/auth_action.php"
+username="14302010017"
+password="linhangyu0303"
+ip="10.88.3.93"
+curl $URL --insecure --data "action=login&username=$username&password=$password&ac_id=1&user_ip=$ip&nas_ip=&user_mac=&save_me=1&ajax=1" > /dev/null 2>&1
